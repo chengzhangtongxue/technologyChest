796edf196fa88826bb387011ab8ffdf57589fcf2
chengzhangtongxue <834010175@qq.com>
2017-10-23 19:46:43 +0800

    新增项目node文件夹

--- technology/Nodejs/project/node-files/router/router.js
+++ technology/Nodejs/project/node-files/router/router.js
@@ -1,25 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+let file = require('../module/file.js');
 
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for
 
 router.get('/',(req,res) => {
-    res.render('index');
+    var promise = file.getAllDir();
+
+    promise.then((fileArr) => {
+        res.render('index',{
+            fileArr:fileArr
+        });
+    });
 });
 
 router.get('/toAddDir',(req,res) => {
-    
+    res.render('add-dir');
 });
 /**
  *  新增相册名称提交
  */
 router.post('/addDir',(req,res) => {
-    var data = bodyParser.body;
-    console.log(data);
-    res.redirect('/');
+    var dirName = req.body.dirName;
+    console.log(dirName);
+    file.createDir(dirName);
+    res.redirect('/home');
 });
 
-router.use((err,req,res,next) => {
-    res.render('404');
-});
 
-module.export = router;
+module.exports = router;
---
+++ technology/Nodejs/project/node-files/test.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var path = require('path');
+
+// fs.mkdir(__dirname+'/test',function(err) {
+
+// });
+
+console.log(path.resolve(__dirname,'../public/'+'/fwjo/fjwe'));
+console.log(path.resolve(__dirname,'/fwjo/fjwe'));
